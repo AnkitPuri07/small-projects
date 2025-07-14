d178a8fc1a91ca87d58456dbc9d40169ef51ce12
Ankit Puri <your-email@example.com>
2025-07-14 18:07:15 +0545

    made html and css of the project

--- password-generator/src/App.jsx
+++ password-generator/src/App.jsx
@@ -2,13 +2,30 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length, setLength] = useState(8);
+  const [characterAllowed, setCharacterAllowed] = useState(false);
+  const [numberAllowed, setNumberAllowed] = useState(false);
 
   return (
-    <>
-     <p className='bg-blue-500 text-white-700'>hello thiscl is testing</p>
-    </>
-  )
-}
+    <div className="w-screen h-screen flex justify-center items-center bg-gray-100">
+      <div className="container bg-white p-6 rounded shadow-lg">
+   <div>
+    <input
+  type="text"
+  className="bg-blue-100 border border-blue-500 text-blue-900 placeholder-blue-700 
+  focus:outline-none focus:ring-2 focus:ring-blue-400 p-2 rounded w-[50%]"
+/>
 
+  <button className='w-[10%] border border-blue-500 border-2 text-black-900 bg-blue-500 rounded-lg cursor-pointer text-lg'>copy</button>
+  <br /><br />
+    </div> 
+
+<input type="range" />
+<label className='text-yellow-600 text-lg font-bold'>length{length}</label>
+<input type="checkbox" className='ml-4'/><label className='text-yellow-600 text-lg font-bold'>number</label>
+<input type="checkbox" className='ml-4'/><label className='text-yellow-600 text-lg font-bold'>character</label>
+      </div>
+    </div>
+  );
+}
 export default App
