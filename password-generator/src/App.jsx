59ac78f4ebd1da30eb75d05076cb5eac7bbd4146
Ankit Puri <your-email@example.com>
2025-07-14 17:25:24 +0545

    initializing new project called password genertaor

---
+++ password-generator/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <p className='bg-blue-500 text-white-700'>hello thiscl is testing</p>
+    </>
+  )
+}
+
+export default App
